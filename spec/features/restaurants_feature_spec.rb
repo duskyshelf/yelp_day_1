4de8474a6767d5ae2e3bb4bcd597c4a707ad00b2
DuskyShelf <dupsdale@gmail.com>
2015-07-20 16:43:05 +0100

    added update functionality to restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants have been added' do
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
+
   end
 
   context 'restaurants have been added' do
-    before do
-      Restaurant.create(name: 'KFC')
-    end
+
+    before {Restaurant.create name: 'KFC'}
 
     scenario 'display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
     end
+
   end
 
   context 'creating restaurants' do
+
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
       visit '/restaurants'
       click_link 'Add a restaurant'
@@ -30,11 +34,12 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
+
   end
 
-  context 'viewing restaurants' do 
-    
-    let!(:kfc){Restaurant.create(name:'KFC')}
+  context 'viewing restaurants' do
+
+    let!(:kfc){Restaurant.create name: 'KFC' }
 
     scenario 'lets a user view a restaurant' do
        visit '/restaurants'
@@ -42,6 +47,22 @@ feature 'restaurants' do
        expect(page).to have_content 'KFC'
        expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
+
+  end
+
+  context 'editing restaurants' do
+
+    before {Restaurant.create name: 'KFC'}
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+
   end
 
 end
