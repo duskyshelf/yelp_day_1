7c639de9420f7ef62bffbefee5da171765acaf2f
DuskyShelf <dupsdale@gmail.com>
2015-07-21 14:55:26 +0100

    reviews get deleted when restaurant is

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Review, type: :model do
+  it {is_expected.to belong_to :restaurant }
+
+  context 'restaurant deletion' do
+    scenario 'gets deleted when restaurant is deleted' do
+      kfc = Restaurant.create name: 'KFC'
+      review = kfc.reviews.create(thoughts: 'meh', rating: '3')
+      kfc.destroy
+      expect(Review.find_by(thoughts: 'meh')).to eq nil
+
+    end
+  end
 end
